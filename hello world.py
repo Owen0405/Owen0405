864433561ea2dd88e0e0ecdcad977d5f716fc030
Owen0405 <104713526+Owen0405@users.noreply.github.com>
2022-07-12 20:42:23 +0800

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,11 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print "hello world"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+prin ("hello wold")
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    prin ("hello wold")
+NameError: name 'prin' is not defined. Did you mean: 'print'?
+print("hello world")
+hello world
